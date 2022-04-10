c4c6ba6f3d5dde13e3f7be36b57494a4158c353d
Laith Alaboodi <laith.laithalaboodi@gmail.com>
2022-04-10 16:46:03 -0700

    Created a models project

---
+++ ContactWebModels/State.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactWebModels
+{
+    public class State
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name ="State")]
+        [Required(ErrorMessage ="Name of State is required")]
+        [StringLength(ContactManagerConstants.MAX_STATE_NAME_LENGTH)]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage ="Name of Abberviation is required")]
+        [StringLength (ContactManagerConstants.MAX_STATE_ABBR_LENGTH)]
+        public string Abberviation { get; set; }
+    }
+}
